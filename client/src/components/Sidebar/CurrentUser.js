dee72a4d97882bb59b7b379a27ebbe8b54173c8e
elsen yacub <elsenyacub@yahoo.co.id>
2024-07-03 17:33:21 +0700

    move logout button into user menu

--- client/src/components/Sidebar/CurrentUser.js
+++ client/src/components/Sidebar/CurrentUser.js
@@ -3,6 +3,7 @@ import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { BadgeAvatar } from "./index";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Menu, MenuItem } from '@mui/material';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -28,18 +29,40 @@ const useStyles = makeStyles(() => ({
     color: "#95A7C4",
     marginRight: 24,
     opacity: 0.5,
+    cursor: "pointer",
   },
 }));
 
-const CurrentUser = ({ user }) => {
+const CurrentUser = ({ user, logout }) => {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box className={classes.root}>
       <BadgeAvatar photoUrl={user?.photoUrl} online={true} />
       <Box className={classes.subContainer}>
         <Typography className={classes.username}>{user?.username}</Typography>
-        <MoreHorizIcon classes={{ root: classes.ellipsis }} />
+        <MoreHorizIcon classes={{ root: classes.ellipsis }} onClick={handleClick} />
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={() => { logout(); handleClose(); }}>Logout</MenuItem>
+        </Menu>
       </Box>
     </Box>
   );
